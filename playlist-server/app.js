1f623bc5e77421720f5b257f43eb70242f770e41
Michael <stall84@gmail.com>
2021-03-05 17:07:21 -0500

    Changed test route format

--- playlist-server/app.js
+++ playlist-server/app.js
@@ -33,13 +33,13 @@ app.get('/test', (req,res) => {
     var status = mongoose.connection.readyState;
     console.log(`Mongo Connection Status: ${status}`);
     switch (status) {
-        case 0 : res.send('Disconnected')
+        case 0 : res.send('<h2 style="color: red">Disconnected</h2>')
         break;
-        case 1 : res.send('Connected')
+        case 1 : res.send('<h2 style="color: green">Connected</h2>')
         break;
-        case 2 : res.send('Connecting...')
+        case 2 : res.send('<h2 style="color: yellow">Connecting...</h2>')
         break;
-        case 3 : res.send('Disconnecting...')
+        case 3 : res.send('<h2 style="color: yellow">Disconnecting...</h2>')
         break;
     }
 })
